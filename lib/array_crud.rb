102164b75da301b8e4370114b51e807de1921abc
Anthony Holder <anthonyh202x@gmail.com>
2020-01-03 04:15:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,6 +1,6 @@
 def create_an_empty_array
-  empty_array = [];
-  empty_array;
+  empty_array = []
+  empty_array
 end
 
 def create_an_array
@@ -8,35 +8,35 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array << element;
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift(element);
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  array.pop();
+  array.pop()
 end
 
 def remove_element_from_start_of_array(array)
-  array.delete_at(0);
+  array.delete_at(0)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array[index_number];
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  array.first;
+  array.first
 end
 
 def retrieve_last_element_from_array(array)
-  array.last;
+  array.last
 end
 
 def update_element_from_index(array, index_number, element)
-  array[index_number] = element;
+  array[index_number] = element
 end
 
 
